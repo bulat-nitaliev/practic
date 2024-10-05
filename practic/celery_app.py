2b2dfaec01ad8d3ce59b4cd8bcb24abe30214722
bulat <nibulat924@gmail.com>
2024-10-05 20:11:45 +0400

    practic

--- practic/celery_app.py
+++ practic/celery_app.py
@@ -20,6 +20,6 @@ app.conf.beat_schedule = {
     },
     'send-spam3': {
         'task' : 'general.tasks.send_graf',
-        'schedule': crontab(minute=30, hour=22),
+        'schedule': crontab(minute=30, hour=19),
     },
 }
