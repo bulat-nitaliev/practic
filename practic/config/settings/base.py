dac7b87cb8f82fe8443c3ed4de73fef3b5c1bf57
bulat <nibulat924@gmail.com>
2024-11-21 22:50:52 +0400

    env

--- practic/config/settings/base.py
+++ practic/config/settings/base.py
@@ -31,7 +31,7 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'sssssssssssssffffffffffffffffeeeee'#config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
